64cba9be0f6774c6ebf0c33674d4bf9b8fef0007
narcpat <narcissearts@gmail.com>
2022-07-26 21:14:31 -0400

    updated contact link

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -38,7 +38,7 @@ function Contact() {
   };
 
   return (
-    <section>
+    <section id="contact">
       <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
